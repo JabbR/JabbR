59dc45b29441a3abbe951608402152adb1fa8dee
David Fowler <davidfowl@gmail.com>
2011-08-23 23:59:54 -0700

    Added generic error handler for tasks

---
+++ Chat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chat.Global" Language="C#" %>
---
+++ Chat/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Threading.Tasks;
+
+namespace Chat {
+    public class Global : System.Web.HttpApplication {
+
+        protected void Application_Start() {
+            TaskScheduler.UnobservedTaskException += (sender, e) => {
+                Elmah.ErrorSignal.Get(this).Raise(e.Exception.GetBaseException());
+                e.SetObserved();
+            };
+        }
+    }
+}
