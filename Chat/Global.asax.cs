7d943062b09371bf024d798c1a2428b4d953e6f0
David Fowler <davidfowl@gmail.com>
2011-08-30 13:18:55 -0700

    Added more exception detection

--- Chat/Global.asax.cs
+++ Chat/Global.asax.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
 using System.Threading.Tasks;
+using Microsoft.CSharp.RuntimeBinder;
 
 namespace Chat {
     public class Global : System.Web.HttpApplication {
 
         protected void Application_Start() {
+            AppDomain.CurrentDomain.FirstChanceException += (sender, e) => {
+                var ex = e.Exception.GetBaseException();
+                if (!(ex is InvalidOperationException) && 
+                    !(ex is RuntimeBinderException) && 
+                    !(ex is MissingMethodException)) {
+                    Elmah.ErrorSignal.Get(this).Raise(ex);
+                }
+            };
+
             TaskScheduler.UnobservedTaskException += (sender, e) => {
                 Elmah.ErrorSignal.Get(this).Raise(e.Exception.GetBaseException());
                 e.SetObserved();
