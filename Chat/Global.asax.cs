5026708e3afaa302ae0bf182490bdcd06cd224ad
David Fowler <davidfowl@gmail.com>
2011-09-05 15:51:13 -0700

    Make tracing signals a config setting

--- Chat/Global.asax.cs
+++ Chat/Global.asax.cs
@@ -1,24 +1,34 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CSharp.RuntimeBinder;
 using SignalR;
 using SignalR.Infrastructure;
-using SignalR.Transports;
+using System.Configuration;
 
 namespace Chat {
     public class Global : System.Web.HttpApplication {
-        protected void Application_Start() {
-            var bus = new TracedSignalBus(new InProcessSignalBus());
-            DependencyResolver.Register(typeof(ISignalBus), () => bus);
+        public static DateTimeOffset Started = DateTimeOffset.UtcNow;
 
-            var store = new TracedMessageStore(new InProcessMessageStore());
-            DependencyResolver.Register(typeof(IMessageStore), () => store);
+        protected void Application_Start() {
+            var setting = ConfigurationManager.AppSettings["traceSignals"];
+            bool traceSignals;
+            if (!String.IsNullOrEmpty(setting) &&
+                Boolean.TryParse(setting, out traceSignals) && 
+                traceSignals) {
+                var bus = new TracedSignalBus(new InProcessSignalBus());
+                DependencyResolver.Register(typeof(ISignalBus), () => bus);
+
+                var store = new TracedMessageStore(new InProcessMessageStore());
+                DependencyResolver.Register(typeof(IMessageStore), () => store);
+            }
 
             AppDomain.CurrentDomain.FirstChanceException += (sender, e) => {
                 var ex = e.Exception.GetBaseException();
                 if (!(ex is InvalidOperationException) && 
                     !(ex is RuntimeBinderException) && 
-                    !(ex is MissingMethodException)) {
+                    !(ex is MissingMethodException) &&
+                    !(ex is ThreadAbortException)) {
                     Elmah.ErrorSignal.Get(this).Raise(ex);
                 }
             };
