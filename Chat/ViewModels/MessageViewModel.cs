a75cbe886c2144075df3d4c75a4b69b7671067af
Kiliman <kiliman@systemex.net>
2011-11-16 11:59:31 -0500

    Implement multi-room chat

--- Chat/ViewModels/MessageViewModel.cs
+++ Chat/ViewModels/MessageViewModel.cs
@@ -5,17 +5,19 @@ namespace Chat.ViewModels
 {
     public class MessageViewModel
     {
-        public MessageViewModel(ChatMessage message)
+        public MessageViewModel(ChatMessage message, ChatRoom room)
         {
             Id = message.Id;
             Content = message.Content;
             User = new UserViewModel(message.User);
             When = message.When;
+            Room = room.Name;
         }
 
         public string Id { get; set; }
         public string Content { get; set; }
         public DateTimeOffset When { get; set; }
         public UserViewModel User { get; set; }
+        public string Room { get; set; }
     }
 }
--- Chat/ViewModels/UserViewModel.cs
+++ Chat/ViewModels/UserViewModel.cs
@@ -4,17 +4,23 @@ namespace Chat.ViewModels
 {
     public class UserViewModel
     {
-        public UserViewModel(ChatUser user)
+        public UserViewModel(ChatUser user) : this (user, null)
+        {
+        }
+
+        public UserViewModel(ChatUser user, ChatRoom room)
         {
             Name = user.Name;
             Hash = user.Hash;
             Id = user.Id;
             Active = user.Active;
+            Room = room == null ? null : room.Name;
         }
 
         public string Name { get; set; }
         public string Hash { get; set; }
         public string Id { get; set; }
         public bool Active { get; set; }
+        public string Room { get; set; }
     }
 }
