3db0e3c8954625f77234c480bbc6f1b2740a4967
David Fowler <davidfowl@gmail.com>
2011-11-24 19:08:45 -0800

    Fixed the migration finally :D (I hope :O)

---
+++ Chat/Migrations/201111250306100_RoomOwners.cs
@@ -0,0 +1,19 @@
+namespace Chat.Models.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class RoomOwners : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("ChatRooms", "Owner_Key", c => c.Int(nullable: true));
+            AddForeignKey("ChatRooms", "Owner_Key", "ChatUsers", "Key");
+        }
+
+        public override void Down()
+        {
+            DropForeignKey("ChatRooms", "Owner_Key", "ChatUsers", "Key");
+            DropColumn("ChatRooms", "Owner_Key");
+        }
+    }
+}
