f78251e89c145af7a03276c3842360a65b9a4eaa
David Fowler <davidfowl@gmail.com>
2011-11-24 17:17:38 -0800

    Fixed migration (hopefully)

--- Chat/Migrations/201111250044435_RoomOwners.cs
+++ Chat/Migrations/201111250044435_RoomOwners.cs
@@ -12,24 +12,10 @@ namespace Chat.Models.Migrations
             AddForeignKey("ChatRooms", "ChatUser_Key", "ChatUsers", "Key");
             AddForeignKey("ChatRooms", "Owner_Key", "ChatUsers", "Key");
             AddForeignKey("ChatUsers", "ChatRoom_Key", "ChatRooms", "Key");
-            DropForeignKey("ChatUserChatRooms", "ChatUser_Key", "ChatUsers", "Key");
-            DropForeignKey("ChatUserChatRooms", "ChatRoom_Key", "ChatRooms", "Key");
-            DropTable("ChatUserChatRooms");
         }
         
         public override void Down()
-        {
-            CreateTable(
-                "ChatUserChatRooms",
-                c => new
-                    {
-                        ChatUser_Key = c.Int(nullable: false),
-                        ChatRoom_Key = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => new { t.ChatUser_Key, t.ChatRoom_Key });
-            
-            AddForeignKey("ChatUserChatRooms", "ChatRoom_Key", "ChatRooms", "Key", cascadeDelete: true);
-            AddForeignKey("ChatUserChatRooms", "ChatUser_Key", "ChatUsers", "Key", cascadeDelete: true);
+        {            
             DropForeignKey("ChatUsers", "ChatRoom_Key", "ChatRooms", "Key");
             DropForeignKey("ChatRooms", "Owner_Key", "ChatUsers", "Key");
             DropForeignKey("ChatRooms", "ChatUser_Key", "ChatUsers", "Key");
