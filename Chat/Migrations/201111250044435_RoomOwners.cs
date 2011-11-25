a692f8ae14b99864b0ef6856e1e632d52d7c5a29
David Fowler <davidfowl@gmail.com>
2011-11-24 17:43:39 -0800

    Trying again

--- Chat/Migrations/201111250044435_RoomOwners.cs
+++ Chat/Migrations/201111250044435_RoomOwners.cs
@@ -7,16 +7,12 @@ namespace Chat.Models.Migrations
         public override void Up()
         {
             AddColumn("ChatRooms", "Owner_Key", c => c.Int());
-            AddColumn("ChatUsers", "ChatRoom_Key", c => c.Int());
             AddForeignKey("ChatRooms", "Owner_Key", "ChatUsers", "Key");
-            AddForeignKey("ChatUsers", "ChatRoom_Key", "ChatRooms", "Key");
         }
         
         public override void Down()
         {            
-            DropForeignKey("ChatUsers", "ChatRoom_Key", "ChatRooms", "Key");
             DropForeignKey("ChatRooms", "Owner_Key", "ChatUsers", "Key");
-            DropColumn("ChatUsers", "ChatRoom_Key");
             DropColumn("ChatRooms", "Owner_Key");
         }
     }
