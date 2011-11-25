51d5808041d4cfdda6d0dce05b0d5bb77151c553
David Fowler <davidfowl@gmail.com>
2011-11-24 17:33:55 -0800

    Try to fix migrations again :(

--- Chat/Migrations/201111250044435_RoomOwners.cs
+++ Chat/Migrations/201111250044435_RoomOwners.cs
@@ -6,10 +6,8 @@ namespace Chat.Models.Migrations
     {
         public override void Up()
         {
-            AddColumn("ChatRooms", "ChatUser_Key", c => c.Int());
             AddColumn("ChatRooms", "Owner_Key", c => c.Int());
             AddColumn("ChatUsers", "ChatRoom_Key", c => c.Int());
-            AddForeignKey("ChatRooms", "ChatUser_Key", "ChatUsers", "Key");
             AddForeignKey("ChatRooms", "Owner_Key", "ChatUsers", "Key");
             AddForeignKey("ChatUsers", "ChatRoom_Key", "ChatRooms", "Key");
         }
@@ -18,10 +16,8 @@ namespace Chat.Models.Migrations
         {            
             DropForeignKey("ChatUsers", "ChatRoom_Key", "ChatRooms", "Key");
             DropForeignKey("ChatRooms", "Owner_Key", "ChatUsers", "Key");
-            DropForeignKey("ChatRooms", "ChatUser_Key", "ChatUsers", "Key");
             DropColumn("ChatUsers", "ChatRoom_Key");
             DropColumn("ChatRooms", "Owner_Key");
-            DropColumn("ChatRooms", "ChatUser_Key");
         }
     }
 }
