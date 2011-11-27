ef29905f217f4a8c72b2ccab1822bd9f8e08db5a
David Fowler <davidfowl@gmail.com>
2011-11-27 05:05:58 -0800

    Remove bad test

--- JabbR.Test/ChatTest.cs
+++ JabbR.Test/ChatTest.cs
@@ -29,38 +29,5 @@ namespace JabbR.Test
             Assert.Equal(versionString, clientState["version"]);
             Assert.False(result);
         }
-
-        [Fact]
-        public void JoinCallsAddUserIfValidUserIdInCookieAndUserList()
-        {
-            var repository = new InMemoryRepository();
-            var user = new ChatUser
-            {
-                Id = "1234",
-                Name = "John",
-                Hash = "Hash"
-            };
-            repository.Add(user);
-
-            var chat = new Chat(repository);
-            var connection = new Mock<IConnection>();
-            var prinicipal = new Mock<IPrincipal>();
-            var cookies = new HttpCookieCollection();
-            cookies.Add(new HttpCookie("userid", "1234"));
-            var clientState = new TrackingDictionary();
-            string clientId = "20";
-            chat.Agent = new ClientAgent(connection.Object, "Chat");
-            chat.Caller = new SignalAgent(connection.Object, clientId, "Chat", clientState);
-            chat.Context = new HubContext(clientId, cookies, prinicipal.Object);
-
-            chat.Join();
-
-            Assert.Equal("1234", clientState["id"]);
-            Assert.Equal("John", clientState["name"]);
-            Assert.Equal("Hash", clientState["hash"]);
-            Assert.Equal("20", user.ClientId);
-            // Need a better way to verify method name and arguments
-            connection.Verify(m => m.Broadcast("Chat.20", It.IsAny<object>()), Times.Once());
-        }
     }
 }
