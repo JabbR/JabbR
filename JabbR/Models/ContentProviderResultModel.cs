39158e402506b0f4e8811c51f42f4b09ef378cf3
sethwebster <sethwebster@gmail.com>
2011-12-03 22:28:36 -0700

    Added ContentResultModel

---
+++ JabbR/Models/ContentProviderResultModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JabbR.Models
+{
+    public class ContentProviderResultModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
