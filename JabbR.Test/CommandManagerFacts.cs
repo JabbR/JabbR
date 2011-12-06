45a72bfc2f50b66caabc47f83a982448b6e47c96
daanl <daanleduc@hotmail.com>
2011-12-06 08:36:51 +0100

    sorting usings statements for captain fowler

--- JabbR.Test/CommandManagerFacts.cs
+++ JabbR.Test/CommandManagerFacts.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using JabbR.Commands;
 using JabbR.Infrastructure;
@@ -6,7 +7,6 @@ using JabbR.Models;
 using JabbR.Services;
 using Moq;
 using Xunit;
-using System.Collections.Generic;
 
 namespace JabbR.Test
 {
