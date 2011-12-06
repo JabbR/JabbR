ac15fccfdd2739dd472e0206b0215ab2e09c5d8e
sethwebster <sethwebster@gmail.com>
2011-12-06 08:02:21 -0700

    refactored for new extract parameters

--- JabbR/ContentProviders/EmbedContentProvider.cs
+++ JabbR/ContentProviders/EmbedContentProvider.cs
@@ -8,30 +8,10 @@ namespace JabbR.ContentProviders
 {
     public abstract class EmbedContentProvider : CollapsibleContentProvider
     {
-        public virtual Regex MediaUrlRegex
-        {
-            get
-            {
-                return null;
-            }
-        }
         public abstract IEnumerable<string> Domains { get; }
         public abstract string MediaFormatString { get; }
 
-        protected virtual IEnumerable<object> ExtractParameters(Uri responseUri)
-        {
-            if (MediaUrlRegex != null)
-            {
-                return MediaUrlRegex.Match(responseUri.AbsoluteUri)
-                                    .Groups
-                                    .Cast<Group>()
-                                    .Skip(1)
-                                    .Select(g => g.Value)
-                                    .Where(v => !String.IsNullOrEmpty(v));
-            }
-            return null;
-        }
-
+       
         protected override ContentProviderResultModel GetCollapsibleContent(HttpWebResponse response)
         {
             var args = ExtractParameters(response.ResponseUri);
