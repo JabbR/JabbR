a6b77583d0ad6f7512b9b5bfa166ac35edf26a03
sethwebster <sethwebster@gmail.com>
2011-12-08 20:53:58 -0700

    fixed index.htm formatting and verbosified javascript

--- JabbR/Chat.pinnedWindows.js
+++ JabbR/Chat.pinnedWindows.js
@@ -11,37 +11,43 @@
             }
         },
         handlePopOut: function (el) {
-            $('#page').height($(window).height()).width($(window).width());
-            var divId = window.chat.utility.randomUniqueId('pop-out-');
-            el.attr('floating', 'true')
-                    .attr('prev-id', collapsibleUtility.getPrevId(el.parent().prev()))
-                    .parent()
-                    .wrap('<div id=' + divId + '/>').parent()
-                        .addClass('collapsible_wrapper')
-                        .appendTo('#page')
-                        .draggable({ containment: '#page', scroll: false });
-            $('#' + divId).css('position', 'absolute');
+            collapsibleUtility.ensureContainmentContainerSize();
+            var popoutWrapperDivId = window.chat.utility.randomUniqueId('pop-out-');
+            // mark the element as floating          
+            el.attr('floating', 'true');
+            // to put the content back where it was, we'll grab the previous element
+            // and save its ID on the clicked pin -- if the elemented doesn't have an
+            // ID, we'll give it one.
+            el.attr('prev-id', collapsibleUtility.getPrevId(el.parent().prev()));
+            // Now we will wrap our content in a div to give use greater control 
+            // over the popout
+            el.parent().wrap('<div id="' + popoutWrapperDivId + '" class="collapsible_wrapper" />');
+            // Add our new element to the end of the #page element and make it draggable
+            el.parent().parent().appendTo('#page').draggable({ containment: '#page', scroll: false });
+            el.parent().parent().css('position', 'absolute');
         },
         handlePopIn: function (el) {
-            $('#page').height($(window).height()).width($(window).width());
+            // mark the element as not floating
             el.attr('floating', 'false');
-            $(el.parent().parent().html()).
-                    insertAfter('#' + el.attr('prev-id'));
+            // here, to ensure that the draggable stuff is
+            // completely removed, we grab the inner html of our wrapper
+            // and stick it back into it's original place
+            $(el.parent().parent().html()).insertAfter('#' + el.attr('prev-id'));
+            // Remove our wrapper to keep the DOM clean
             el.parent().parent().remove();
+        },
+        ensureContainmentContainerSize: function () {
+            $('#page').height($(window).height()).width($(window).width());
         },
         getPrevId: function (el) {
             var prevId = el.attr('id');
-            if (prevId === null || prevId === '') {
+            if (prevId === undefined || prevId === '') {
                 prevId = window.chat.utility.randomUniqueId('prev-pop-out-');
                 el.attr('id', prevId);
             }
             return prevId;
-        },
-        hookPins: function () {
-            $(document).off('click', 'div.collapsible_pin');
-            $(document).on('click', 'div.collapsible_pin', collapsibleUtility.handlePinClick);
         }
     }
-    collapsibleUtility.hookPins();
+    $(document).on('click', 'div.collapsible_pin', collapsibleUtility.handlePinClick);
 
 })(jQuery, window, chat.ui);
