21487e6da24ff44f22d7948a018fad34b1a94a2d
sethwebster <sethwebster@gmail.com>
2011-12-08 20:03:32 -0700

    Fixed formatting of index.htm, replaced " with '

--- JabbR/Chat.pinnedWindows.js
+++ JabbR/Chat.pinnedWindows.js
@@ -1,9 +1,9 @@
 ﻿(function ($, window, ui) {
-    "use strict";
+    'use strict';
 
     var collapsibleUtility = {
         handlePinClick: function () {
-            if ($(this).attr("floating") == "true") {
+            if ($(this).attr('floating') == 'true') {
                 collapsibleUtility.handlePopIn($(this));
             }
             else {
@@ -11,29 +11,29 @@
             }
         },
         handlePopOut: function (el) {
-            $("#page").height($(window).height()).width($(window).width());
-            var divId = window.chat.utility.randomUniqueId("pop-out-");
-            el.attr("floating", true)
-                    .attr("prev-id", collapsibleUtility.getPrevId(el.parent().prev()))
+            $('#page').height($(window).height()).width($(window).width());
+            var divId = window.chat.utility.randomUniqueId('pop-out-');
+            el.attr('floating', true)
+                    .attr('prev-id', collapsibleUtility.getPrevId(el.parent().prev()))
                     .parent()
-                    .wrap("<div id='" + divId + "'/>").parent()
-                        .addClass("collapsible_wrapper")
-                        .appendTo("#page")
-                        .draggable({ containment: "#page", scroll: false });
-            $("#" + divId).css("position", "absolute");
+                    .wrap('<div id=' + divId + '/>').parent()
+                        .addClass('collapsible_wrapper')
+                        .appendTo('#page')
+                        .draggable({ containment: '#page', scroll: false });
+            $('#' + divId).css('position', 'absolute');
         },
         handlePopIn: function (el) {
-            $("#page").height($(window).height()).width($(window).width());
-            el.attr("floating", false);
+            $('#page').height($(window).height()).width($(window).width());
+            el.attr('floating', false);
             $(el.parent().parent().html()).
-                    insertAfter("#" + el.attr("prev-id"));
+                    insertAfter('#' + el.attr('prev-id'));
             el.parent().parent().remove();
         },
         getPrevId: function (el) {
-            var prevId = el.attr("id");
-            if (prevId == null || prevId == "") {
-                prevId = window.chat.utility.randomUniqueId("prev-pop-out-");
-                el.attr("id", prevId);
+            var prevId = el.attr('id');
+            if (prevId == null || prevId == '') {
+                prevId = window.chat.utility.randomUniqueId('prev-pop-out-');
+                el.attr('id', prevId);
             }
             return prevId;
         },
