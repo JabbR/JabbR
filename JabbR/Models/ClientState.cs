03cc2ef55bcb06a031cb7b8d4345183acf7d4e49
daanl <daanleduc@hotmail.com>
2011-12-11 09:36:36 +0100

    Adding tests for chat.cs

---
+++ JabbR/Models/ClientState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JabbR.Models
+{
+    public class ClientState
+    {
+        public string UserId { get; set; }
+        public string ActiveRoom { get; set; }
+    }
+}
