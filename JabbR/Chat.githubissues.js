076770077419db57c202d41d723673ad8a35207c
sethwebster <sethwebster@gmail.com>
2011-12-15 14:28:26 -0700

    Added MARKDOWN (md) parsing for Issues

--- JabbR/Chat.githubissues.js
+++ JabbR/Chat.githubissues.js
@@ -10,17 +10,20 @@
             .removeClass('git-hub-issue-' + issue.data.number);
 
         // Process the template, and add it in to the div.
+        var  converter = new Markdown.Converter().makeHtml;
+        issue.data.body = converter(issue.data.body);
+
         $('#github-issues-template').tmpl(issue.data).appendTo(elements);
+
         $('.js-relative-date').timeago();
         // If near the end, scroll.
         if (nearEnd) {
             ui.scrollToBottom();
         }
-        elements.append('<script src="https://api.github.com/users/'+issue.data.user.login+'?callback=addGitHubIssuesUser"></script>');
-        if (issue.data.assignee!=undefined)
-        {
-                elements.append('<script src="https://api.github.com/users/'+issue.data.assignee.login+'?callback=addGitHubIssuesUser"></script>');
-        }   
+        elements.append('<script src="https://api.github.com/users/' + issue.data.user.login + '?callback=addGitHubIssuesUser"></script>');
+        if (issue.data.assignee != undefined) {
+            elements.append('<script src="https://api.github.com/users/' + issue.data.assignee.login + '?callback=addGitHubIssuesUser"></script>');
+        }
     };
 
     window.addGitHubIssuesUser = function (user) {
