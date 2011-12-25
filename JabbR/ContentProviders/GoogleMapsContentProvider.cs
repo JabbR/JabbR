8c0c788ce8491d8c538fd0ec43748eb5c0da45e3
David Fowler <davidfowl@gmail.com>
2011-12-25 03:22:16 -0800

    Changed the GoogleMapsContentProvider to use iframe instead of an image. - Extract parameter from query string rather then using regex.

--- JabbR/ContentProviders/GoogleMapsContentProvider.cs
+++ JabbR/ContentProviders/GoogleMapsContentProvider.cs
@@ -1,21 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web;
 using JabbR.ContentProviders.Core;
 
 namespace JabbR.ContentProviders
 {
     public class GoogleMapsContentProvider : EmbedContentProvider
     {
-        private static readonly Regex _mapQueryRegex = new Regex(@".&hnear=(.*)\&[tg].*");
-
-        protected override Regex ParameterExtractionRegex
-        {
-            get
-            {
-                return _mapQueryRegex;
-            }
-        }
-
         public override IEnumerable<string> Domains
         {
             get { yield return "http://maps.google.com"; }
@@ -25,8 +17,39 @@ namespace JabbR.ContentProviders
         {
             get
             {
-                return @"<a href=""http://maps.google.com?q={0}"">Google Maps</a><br/><img src=""http://maps.googleapis.com/maps/api/staticmap?center={0}&zoom=12&size=500x400&sensor=false""/>";
+                return @"<iframe width=""500"" height=""350"" frameborder=""0"" scrolling=""no"" marginheight=""0"" marginwidth=""0"" src=""http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl={1}&amp;geocode=&amp;q={0}&amp;aq=0&amp;sll={4}&amp;sspn={5}&amp;vpsrc={6}&amp;ie=UTF8&amp;hq=&amp;hnear={7}&amp;ll={2}&amp;spn={3}&amp;t={8}&amp;z={9}&amp;output=embed""></iframe><br /><small><a href=""http://maps.google.com/maps?f=q&amp;source=embed&amp;hl={1}&amp;geocode=&amp;q={0}&amp;aq=0&amp;sll={4}&amp;sspn={5}&amp;vpsrc={6}&amp;ie=UTF8&amp;hq=&amp;hnear={7}&amp;ll={2}&amp;spn={3}&amp;t={8}&amp;z={9}"" style=""color:#0000FF;text-align:left"" target=""_blank"">View Larger Map</a></small>";
             }
+        }
+
+        protected override IList<string> ExtractParameters(Uri responseUri)
+        {
+            var queryString = HttpUtility.ParseQueryString(responseUri.Query);
+            string query = queryString["q"];
+            string hl = queryString["hl"];
+            string ll = queryString["ll"];
+            string spn = queryString["spn"];
+            string sll = queryString["sll"];
+            string sspn = queryString["sspn"];
+            string vpsrc = queryString["vpsrc"];
+            string hnear = queryString["hnear"];
+            string t = queryString["t"];
+            string z = queryString["z"];
+
+            if (String.IsNullOrEmpty(ll))
+            {
+                return null;
+            }
+
+            return new List<string>() { query, hl, ll, spn, sll, sspn, vpsrc, hnear, t, z };
+        }
+
+
+        protected override ContentProviderResultModel GetCollapsibleContent(HttpWebResponse response)
+        {
+            ContentProviderResultModel content = base.GetCollapsibleContent(response);
+            var queryString = HttpUtility.ParseQueryString(response.ResponseUri.Query);
+            content.Title = queryString["q"] ?? "Google Maps";
+            return content;
         }
     }
 }
