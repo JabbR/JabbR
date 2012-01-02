624fa06befa569d815666df349ecb20dda29bcf4
David Fowler <davidfowl@gmail.com>
2012-01-02 05:29:59 -0800

    Fix bug in adding new flag logic.

--- JabbR/Chat.ui.js
+++ JabbR/Chat.ui.js
@@ -467,6 +467,7 @@
         var $flag = $user.find('.flag');
 
         $flag.removeClass();
+        $flag.addClass('flag');
         $flag.removeAttr('title');
 
         $flag.addClass(userViewModel.flagClass);
