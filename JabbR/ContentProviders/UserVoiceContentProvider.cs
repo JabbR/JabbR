8a351de5b53f6aa36269035b5dae1ad9d6ee05a3
David Christiansen <code@davedoes.net>
2012-01-12 22:41:01 +0000

    Corrected code styling.

--- JabbR/ContentProviders/UserVoiceContentProvider.cs
+++ JabbR/ContentProviders/UserVoiceContentProvider.cs
@@ -4,29 +4,37 @@ using System.Net;
 using JabbR.ContentProviders.Core;
 using Newtonsoft.Json;
 
-namespace JabbR.ContentProviders {
-    public class UserVoiceContentProvider : CollapsibleContentProvider {
+namespace JabbR.ContentProviders
+{
+    public class UserVoiceContentProvider : CollapsibleContentProvider
+    {
+        private static readonly string _uservoiceAPIURL = "http://{0}/api/v1/oembed.json?url={1}";
 
-        protected override ContentProviderResultModel GetCollapsibleContent(HttpWebResponse response) {
+        protected override ContentProviderResultModel GetCollapsibleContent(HttpWebResponse response)
+        {
             var article = FetchArticle(response.ResponseUri);
-            return new ContentProviderResultModel() {
-                Title = article.title,
-                Content = article.html
-            };
+            return new ContentProviderResultModel()
+                       {
+                           Title = article.title,
+                           Content = article.html
+                       };
         }
 
-        private static dynamic FetchArticle(Uri url) {
-            const string API = "http://{0}/api/v1/oembed.json?url={1}";
-            var webRequest = (HttpWebRequest)WebRequest.Create(String.Format(API, url.Host, url.AbsoluteUri));
+        private static dynamic FetchArticle(Uri url)
+        {
+            var webRequest = (HttpWebRequest) WebRequest.Create(String.Format(_uservoiceAPIURL, url.Host, url.AbsoluteUri));
             webRequest.Accept = "application/json";
-            using (var webResponse = webRequest.GetResponse()) {
-                using (var sr = new StreamReader(webResponse.GetResponseStream())) {
+            using (var webResponse = webRequest.GetResponse())
+            {
+                using (var sr = new StreamReader(webResponse.GetResponseStream()))
+                {
                     return JsonConvert.DeserializeObject(sr.ReadToEnd());
                 }
             }
         }
 
-        protected override bool IsValidContent(HttpWebResponse response) {
+        protected override bool IsValidContent(HttpWebResponse response)
+        {
             return response.ResponseUri.Host.IndexOf("uservoice.com", StringComparison.OrdinalIgnoreCase) >= 0;
         }
     }
