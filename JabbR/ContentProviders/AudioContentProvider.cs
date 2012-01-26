09ae3d3392414874ce525e3bfb7b4ec7d5534f9d
Matthew Osborn <matthew@osbornm.com>
2012-01-25 17:02:54 -0800

    Changing the Audio Content Provider to not be collapsible, as doing that made it twice as tall.

--- JabbR/ContentProviders/AudioContentProvider.cs
+++ JabbR/ContentProviders/AudioContentProvider.cs
@@ -5,7 +5,7 @@ using JabbR.ContentProviders.Core;
 
 namespace JabbR.ContentProviders
 {
-    public class AduioContentProvider : CollapsibleContentProvider
+    public class AduioContentProvider : IContentProvider
     {
         private static readonly HashSet<string> _audioMimeTypes = new HashSet<string>(StringComparer.OrdinalIgnoreCase) {
             "audio/mpeg",
@@ -13,20 +13,24 @@ namespace JabbR.ContentProviders
             "aduio/ogg"
         };
 
-        protected override ContentProviderResultModel GetCollapsibleContent(HttpWebResponse response)
-        {
-            return new ContentProviderResultModel()
-             {
-                 Content = String.Format(@"<audio controls=""controls"" src=""{0}"">Your browser does not support the audio tag.</audio>", response.ResponseUri),
-                 Title = response.ResponseUri.AbsoluteUri.ToString()
-             };
-        }
-
-        protected override bool IsValidContent(HttpWebResponse response)
+        protected bool IsValidContent(HttpWebResponse response)
         {
             return !String.IsNullOrEmpty(response.ContentType) &&
                     _audioMimeTypes.Contains(response.ContentType);
 
+        }
+
+        public ContentProviderResultModel GetContent(HttpWebResponse response)
+        {
+            if (IsValidContent(response))
+            {
+                return new ContentProviderResultModel()
+                {
+                    Content = String.Format(@"<audio controls=""controls"" src=""{0}"">Your browser does not support the audio tag.</audio>", response.ResponseUri),
+                    Title = response.ResponseUri.AbsoluteUri.ToString()
+                };
+            }
+            return null;
         }
     }
 }
