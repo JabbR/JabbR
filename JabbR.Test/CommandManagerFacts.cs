39f7f7f8055ff381bd74cf6df2953064f20aac12
David Fowler <davidfowl@gmail.com>
2012-01-29 12:13:30 -0800

    Fix flaky test.

--- JabbR.Test/CommandManagerFacts.cs
+++ JabbR.Test/CommandManagerFacts.cs
@@ -2012,6 +2012,13 @@ namespace JabbR.Test
                 repository.Add(room);
                 var service = new ChatService(repository, new Mock<ICryptoService>().Object);
                 var notificationService = new Mock<INotificationService>();
+                var userList = new List<string>();
+                notificationService.Setup(m => m.ListUsers(It.IsAny<ChatRoom>(), It.IsAny<IEnumerable<string>>()))
+                                   .Callback<ChatRoom, IEnumerable<string>>((_, names) =>
+                                   {
+                                       userList.AddRange(names);
+                                   });
+
                 var commandManager = new CommandManager("clientid",
                                                         "1",
                                                         null,
@@ -2021,12 +2028,10 @@ namespace JabbR.Test
 
                 bool result = commandManager.TryHandleCommand("/list room");
 
-                var userList = new List<String>();
-                userList.Add(user.Name);
-                userList.Add(user2.Name);
-
                 Assert.True(result);
-                notificationService.Verify(x => x.ListUsers(room, userList), Times.Once());
+                Assert.Equal(2, userList.Count);
+                Assert.True(userList.Contains("dfowler2"));
+                Assert.True(userList.Contains("dfowler"));
             }
         }
 
@@ -2999,7 +3004,7 @@ namespace JabbR.Test
 
         public class CloseCommand
         {
-            [Fact]    
+            [Fact]
             public void MissingRoomNameThrows()
             {
                 // Arrange.
@@ -3050,7 +3055,7 @@ namespace JabbR.Test
                 InvalidOperationException ex = Assert.Throws<InvalidOperationException>(() => commandManager.TryHandleCommand("/close " + roomName));
                 Assert.Equal("Unable to find room '" + roomName + "'", ex.Message);
             }
-            
+
             [Fact]
             public void CannotCloseARoomIfTheUserIsNotAnOwner()
             {
@@ -3103,7 +3108,7 @@ namespace JabbR.Test
                 };
                 // Add a room owner.
                 room.Owners.Add(user);
-                
+
                 repository.Add(room);
 
                 var service = new ChatService(repository, new Mock<ICryptoService>().Object);
@@ -3118,7 +3123,7 @@ namespace JabbR.Test
                 bool result = commandManager.TryHandleCommand("/close " + roomName);
 
                 Assert.True(result);
-                notificationService.Verify(x => x.CloseRoom(room.Users,  room), Times.Once());
+                notificationService.Verify(x => x.CloseRoom(room.Users, room), Times.Once());
                 Assert.True(room.Closed);
             }
 
