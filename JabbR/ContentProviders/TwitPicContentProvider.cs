5fc52fb793ec9a1bb3edaab371e0569e70bfee5e
Tom Janssens <Tom@corebvba.be>
2012-01-31 14:46:49 +0100

    Using statements sorted and cleaned

--- JabbR/ContentProviders/TwitPicContentProvider.cs
+++ JabbR/ContentProviders/TwitPicContentProvider.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using JabbR.ContentProviders.Core;
-using System.Text.RegularExpressions;
 using System.Net;
+using System.Text.RegularExpressions;
+using JabbR.ContentProviders.Core;
 
 namespace JabbR.ContentProviders
 {
