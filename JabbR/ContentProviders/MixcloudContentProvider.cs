005b59bd490ff5b1a43aca8bda62c0aa3dc0147e
David Christiansen <code@davedoes.net>
2012-02-01 16:02:50 +0000

    Creation of Mixcloud ContentProvider

---
+++ JabbR/ContentProviders/MixcloudContentProvider.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using JabbR.ContentProviders.Core;
+
+namespace JabbR.ContentProviders {
+    public class MixcloudContentProvider : EmbedContentProvider
+    {
+        public override string MediaFormatString
+        {
+            get
+            {
+                return "<object width=\"100%\" height=\"120\"><param name=\"movie\" value=\"http://www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed={0}&embed_uuid=06c5d381-1643-407d-80e7-2812382408e9&stylecolor=1e2671&embed_type=widget_standard\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"wmode\" value=\"opaque\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"http://www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed={0}&embed_uuid=06c5d381-1643-407d-80e7-2812382408e9&stylecolor=1e2671&embed_type=widget_standard\" type=\"application/x-shockwave-flash\" wmode=\"opaque\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"100%\" height=\"120\"></embed></object>";
+            }
+        }
+
+        public override IEnumerable<string> Domains
+        {
+            get
+            {
+                yield return "http://www.mixcloud.com/";
+            }
+        }
+
+        protected override IList<string> ExtractParameters(Uri responseUri)
+        {
+            return new List<string>() { responseUri.AbsoluteUri };
+        }
+    }
+}
