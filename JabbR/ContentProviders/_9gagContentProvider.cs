27cc9f59b3deaba85ca3587e6b8c6c2b6959f48f
Mike Hurtado <mikehc@gmail.com>
2012-04-06 16:31:54 -0700

    #433 - Added support for 9gag as content provider

---
+++ JabbR/ContentProviders/_9gagContentProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using JabbR.ContentProviders.Core;
+
+namespace JabbR.ContentProviders
+{
+    public class _9gagContentProvider : CollapsibleContentProvider
+    {
+        static Regex _9gagUrlRegex = new Regex(@"^http://(www\.)?9gag\.com/gag/(?<Id>\w+)", RegexOptions.IgnoreCase);
+
+        private readonly string _9gagFormatString = @"<a href=""http://9gag.com/gag/{0}""> <img src=""http://d24w6bsrhbeh9d.cloudfront.net/photo/{0}_700b.jpg""></a>";
+
+        protected override Task<ContentProviderResult> GetCollapsibleContent(ContentProviderHttpRequest request)
+        {
+            var match = _9gagUrlRegex.Match(request.RequestUri.AbsoluteUri);
+
+            if (match.Success)
+            {
+                var id = match.Groups["Id"].Value;
+                return TaskAsyncHelper.FromResult(new ContentProviderResult()
+                {
+                    Content = String.Format(_9gagFormatString, id),
+                    Title = request.RequestUri.AbsoluteUri
+                });
+            }
+
+            return TaskAsyncHelper.FromResult<ContentProviderResult>(null);
+        }
+
+        public override bool IsValidContent(Uri uri)
+        {
+            return _9gagUrlRegex.IsMatch(uri.AbsoluteUri);
+        }
+    }
+}
