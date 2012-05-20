c3922e4ac3ed0cad8ea096096596138046235873
David Pendray <davidpendray@gmail.com>
2012-05-20 23:18:09 +0100

    Only perform animations for visible rooms to fix issue with all topics lines being shown on screen at once when page first loads.

--- JabbR/Chat.ui.js
+++ JabbR/Chat.ui.js
@@ -690,9 +690,14 @@
         var topic = roomViewModel.Topic;
         var topicHtml = topic === '' ? 'You\'re chatting in ' + roomViewModel.Name : '<strong>Topic: </strong>' + topic;
         var roomTopic = room.roomTopic;
-        roomTopic.hide();
+        var isVisibleRoom = getCurrentRoomElements().getName() == roomViewModel.Name;
+        if (isVisibleRoom) {
+            roomTopic.hide();
+        }
         roomTopic.html(topicHtml);
-        roomTopic.slideDown('slow');
+        if (isVisibleRoom) {
+            roomTopic.slideDown('slow');
+        }
     }
 
     // Rotating Tips.
