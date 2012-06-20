92d72b8f2b098c208c462b0eaddda72573a416f6
Thomas Johansen <thomasjo@gmail.com>
2012-06-20 23:25:22 +0200

    Add /welcome command.

---
+++ JabbR/Commands/WelcomeCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JabbR.Models;
+using JabbR.Services;
+
+namespace JabbR.Commands
+{
+    [Command("welcome", "")]
+    public class WelcomeCommand : UserCommand
+    {
+        public override void Execute(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
+        {
+            string newWelcome = String.Join(" ", args).Trim();
+            ChatService.ValidateWelcome(newWelcome);
+
+            newWelcome = String.IsNullOrWhiteSpace(newWelcome) ? null : newWelcome;
+
+            ChatRoom room = context.Repository.VerifyUserRoom(context.Cache, callingUser, callerContext.RoomName);
+            context.Service.ChangeWelcome(callingUser, room, newWelcome);
+            context.NotificationService.ChangeWelcome(callingUser, room);
+        }
+    }
+}
