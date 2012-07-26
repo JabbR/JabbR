a2d63b2f2e80598715ce45f21fc7724d526ce3d1
Doug Patti <douglas@fogcreek.com>
2012-07-25 21:03:08 -0400

    Fix to up/down arrow keys in multiline messages
    Previously we prevented the default event to stop the cursor from moving when we
    cycle through messages, but that unintendedly prevents us from scrolling around
    after pasting in a multi-line message. We can return whether a cycle took place
    to know whether we should stop the key from moving the cursor.

--- JabbR/Chat.ui.js
+++ JabbR/Chat.ui.js
@@ -988,12 +988,14 @@
                 var key = ev.keyCode || ev.which;
                 switch (key) {
                     case Keys.Up:
-                        cycleMessage(ui.events.prevMessage);
-                        ev.preventDefault();
+                        if (cycleMessage(ui.events.prevMessage)) {
+                            ev.preventDefault();
+                        }
                         break;
                     case Keys.Down:
-                        cycleMessage(ui.events.nextMessage);
-                        ev.preventDefault();
+                        if (cycleMessage(ui.events.nextMessage)) {
+                            ev.preventDefault();
+                        }
                         break;
                     case Keys.Esc:
                         $(this).val('');
@@ -1011,11 +1013,14 @@
                 }
             });
 
+            // Returns true if a cycle was triggered
             function cycleMessage(messageHistoryDirection) {
                 var currentMessage = $newMessage[0].value;
                 if (currentMessage.length === 0 || lastCycledMessage === currentMessage) {
                     $ui.trigger(messageHistoryDirection);
+                    return true;
                 }
+                return false;
             }
 
             // Auto-complete for user names
