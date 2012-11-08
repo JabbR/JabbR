c98b4130b6357c881944d7db6c1636697aaa6c56
Nick Schonning <nschonni@gmail.com>
2012-11-07 19:36:19 -0500

    Use sparse ViewModel for lobby JSON

---
+++ JabbR/ViewModels/LobbyRoomViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace JabbR.ViewModels
+{
+    public class LobbyRoomViewModel
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public bool Private { get; set; }
+        public bool Closed { get; set; }
+    }
+}
