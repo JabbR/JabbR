8db4afe36034c9285ae9136924c29325b5876271
Nick Schonning <nschonni@gmail.com>
2012-11-07 21:10:32 -0500

    JSHint Chat.emoji.js
    -Creating functions in a loop
    -Unnessessary semicolon

--- JabbR/Chat.emoji.js
+++ JabbR/Chat.emoji.js
@@ -30,10 +30,10 @@
         function parseEmoji(content) {
             for(var key in validAlias) {
                 var regex = new RegExp(key, "g");
-                content = content.replace(regex, function() { return validAlias[key] });
+                content = content.replace(regex, validAlias[key]);
             }
             return content.replace(/:([a-z0-9\+\-_]+):/g, emojiReplacer);
-        };
+        }
 
         function emojiReplacer(str, match) {
             if (validEmoji[match]) {
