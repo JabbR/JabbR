1a42597944e5160fb851f236cfbe46b331ca8d24
David Fowler <davidfowl@gmail.com>
2012-11-13 03:51:35 -0800

    Updated to alpha2.

---
+++ JabbR/App_Start/RegisterHubs.cs
@@ -0,0 +1,18 @@
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hosting.AspNet;
+
+[assembly: PreApplicationStartMethod(typeof(JabbR.RegisterHubs), "Start")]
+
+namespace JabbR
+{
+    public static class RegisterHubs
+    {
+        public static void Start()
+        {
+            // Register the default hubs route: ~/signalr/hubs
+            RouteTable.Routes.MapHubs();            
+        }
+    }
+}
