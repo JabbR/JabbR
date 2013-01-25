84b267fc8593b1a99391e0bf94922cb35d53e5b1
David Fowler <davidfowl@gmail.com>
2013-01-24 23:11:37 -0800

    Flow IPrincipal through to Nancy since it doesn't pick it up at all...

--- JabbR/Infrastructure/JabbRNinjectNancyBootstrapper.cs
+++ JabbR/Infrastructure/JabbRNinjectNancyBootstrapper.cs
@@ -1,4 +1,10 @@
-﻿using Nancy.Bootstrappers.Ninject;
+﻿using System.Collections.Generic;
+using System.Security.Principal;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Bootstrappers.Ninject;
+using Nancy.Owin;
+using Nancy.Security;
 using Ninject;
 
 namespace JabbR.Infrastructure
@@ -15,6 +21,59 @@ namespace JabbR.Infrastructure
         protected override IKernel GetApplicationContainer()
         {
             return _kernel;
+        }
+
+        protected override void ApplicationStartup(IKernel container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            pipelines.BeforeRequest.AddItemToStartOfPipeline(FlowPrincipal);
+        }
+
+        private Response FlowPrincipal(NancyContext context)
+        {
+            var env = Get<IDictionary<string, object>>(context.Items, NancyOwinHost.RequestEnvironmentKey);
+            if (env != null)
+            {
+                var principal = Get<IPrincipal>(env, "server.User");
+                if (principal != null)
+                {
+                    context.CurrentUser = new PrincipalIdentity(principal);
+                }
+            }
+
+            return null;
+        }
+
+        private static T Get<T>(IDictionary<string, object> env, string key)
+        {
+            object value;
+            if (env.TryGetValue(key, out value))
+            {
+                return (T)value;
+            }
+            return default(T);
+        }
+
+        private class PrincipalIdentity : IUserIdentity
+        {
+            public PrincipalIdentity(IPrincipal principal)
+            {
+                // This is actually the Id
+                UserName = principal.Identity.Name;
+            }
+
+            public IEnumerable<string> Claims
+            {
+                get;
+                set;
+            }
+
+            public string UserName
+            {
+                get;
+                set;
+            }
         }
     }
 }
