331555ac63d9604181a3ce304c52464be7c82e5f
David Fowler <davidfowl@gmail.com>
2013-01-27 01:17:14 -0800

    Added support for migrating facebook legacy identities.

--- JabbR/Infrastructure/IdentityUtility.cs
+++ JabbR/Infrastructure/IdentityUtility.cs
@@ -12,6 +12,8 @@ namespace JabbR.Infrastructure
                     return String.Format("http://twitter.com/account/profile?user_id={0}", identity);
                 case "google":
                     return String.Format("https://www.google.com/profiles/{0}", identity);
+                case "facebook":
+                    return String.Format("http://www.facebook.com/profile.php?id={0}", identity);
                 default:
                     break;
             }
