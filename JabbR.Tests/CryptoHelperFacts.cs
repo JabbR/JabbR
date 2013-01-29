a2924ada73e544ba918cbfb0cebc4cbb5f8e8cc6
David Fowler <davidfowl@gmail.com>
2013-01-28 19:42:57 -0800

    Added basic CryptoHelper for symmetric key encryption.

---
+++ JabbR.Tests/CryptoHelperFacts.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using JabbR.Infrastructure;
+using Xunit;
+
+namespace JabbR.Tests
+{
+    public class CryptoHelperFacts
+    {
+        [Fact]
+        public void ProtectCanUnProtect()
+        {
+            byte[] encryptionKey = GenerateRandomBytes();
+            byte[] validationKey = GenerateRandomBytes();
+
+            using (var algo = new AesCryptoServiceProvider())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("Hello World");
+                byte[] payload = CryptoHelper.Protect(encryptionKey, validationKey, algo.IV, bytes);
+
+                byte[] buffer = CryptoHelper.Unprotect(encryptionKey, validationKey, payload);
+
+                Assert.Equal("Hello World", Encoding.UTF8.GetString(buffer));
+            }
+        }
+
+        [Fact]
+        public void WrongValidationKeyWontDecrypt()
+        {
+            byte[] encryptionKey = GenerateRandomBytes();
+            byte[] validationKey = GenerateRandomBytes();
+
+            using (var algo = new AesCryptoServiceProvider())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("Hello World");
+                byte[] payload = CryptoHelper.Protect(encryptionKey, validationKey, algo.IV, bytes);
+                Assert.Throws<InvalidOperationException>(() => CryptoHelper.Unprotect(encryptionKey, GenerateRandomBytes(), payload));
+            }
+        }
+
+        [Fact]
+        public void WrongPayloadDecrypt()
+        {
+            byte[] encryptionKey = GenerateRandomBytes();
+            byte[] validationKey = GenerateRandomBytes();
+
+            using (var algo = new AesCryptoServiceProvider())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("Hello World");
+                CryptoHelper.Protect(encryptionKey, validationKey, algo.IV, bytes);
+                Assert.Throws<InvalidOperationException>(() => CryptoHelper.Unprotect(encryptionKey, validationKey, GenerateRandomBytes()));
+            }
+        }
+
+        private byte[] GenerateRandomBytes(int n = 32)
+        {
+            using (var cryptoProvider = new RNGCryptoServiceProvider())
+            {
+                var bytes = new byte[n];
+                cryptoProvider.GetBytes(bytes);
+                return bytes;
+            }
+        }
+    }
+}
---
+++ JabbR/Infrastructure/CryptoHelper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace JabbR.Infrastructure
+{
+    public class CryptoHelper
+    {
+        // AES uses a 16 byte IV
+        private const int IVLength = 16;
+
+        // HMAC 256
+        private const int HMacLength = 32;
+
+        public static byte[] Protect(byte[] encryptionKey, byte[] validationKey, byte[] initializationVector, byte[] plainText)
+        {            
+            using (var provider = new AesCryptoServiceProvider())
+            {
+                using (ICryptoTransform transform = provider.CreateEncryptor(encryptionKey, initializationVector))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        ms.Write(initializationVector, 0, initializationVector.Length);
+                        using (var cryptoStream = new CryptoStream(ms, transform, CryptoStreamMode.Write))
+                        {
+                            // Encrypted payload
+                            cryptoStream.Write(plainText, 0, plainText.Length);
+                            cryptoStream.FlushFinalBlock();
+
+                            // Compute signature
+                            using (var sha = new HMACSHA256(validationKey))
+                            {
+                                checked
+                                {
+                                    byte[] signature = sha.ComputeHash(ms.GetBuffer(), 0, (int)ms.Length);
+
+                                    // Write the signature to the paylod
+                                    ms.Write(signature, 0, signature.Length);
+
+                                    // Final bytes
+                                    return ms.ToArray();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static byte[] Unprotect(byte[] encryptionKey, byte[] validationKey, byte[] payload)
+        {
+            using (var provider = new AesCryptoServiceProvider())
+            {
+                var initializationVector = new byte[IVLength];
+
+                using (var sha = new HMACSHA256(validationKey))
+                {
+                    checked
+                    {
+                        // The length of the unsigned payload
+                        int payloadOffset = payload.Length - HMacLength;
+
+                        // Computer the hash of the IV and cipher text for validation
+                        byte[] hash = sha.ComputeHash(payload, 0, payloadOffset);
+
+                        // Make sure they match
+                        ValidateHashBytes(payload, hash, payloadOffset);
+
+                        Buffer.BlockCopy(payload, 0, initializationVector, 0, initializationVector.Length);
+                    }
+                }
+
+                using (ICryptoTransform transform = provider.CreateDecryptor(encryptionKey, initializationVector))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        using (var cryptoStream = new CryptoStream(ms, transform, CryptoStreamMode.Write))
+                        {
+                            checked
+                            {
+                                cryptoStream.Write(payload, IVLength, payload.Length - (HMacLength + IVLength));
+                                cryptoStream.FlushFinalBlock();
+
+                                return ms.ToArray();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static void ValidateHashBytes(byte[] payload, byte[] hash, int payloadOffset)
+        {
+            // Just because
+            checked
+            {
+                bool ok = true;
+
+                // DO NOT Change this CODE! It's O(HMacLength) for a reason
+                for (int i = 0; i < HMacLength; i++)
+                {
+                    ok &= hash[i] == payload[i + payloadOffset];
+                }
+
+                if (!ok)
+                {
+                    // Tsk tsk tsk, stop tampering with my data (BAD TOUCH!)
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
