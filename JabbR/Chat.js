553eec829e1d3dba04947ae5dee536cf8d90760b
Henrik Andersson <alfhenrik@gmail.com>
2013-03-26 19:18:32 +1000

    Fix up new lines

--- JabbR/Chat.js
+++ JabbR/Chat.js
@@ -161,9 +161,11 @@
         interval = Math.floor(seconds / 86400);
         if (interval > 1) { return interval + "d"; }
 
-        interval = Math.floor(seconds / 3600); if (interval > 1) { return interval + "h"; }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) { return interval + "h"; }
 
-        interval = Math.floor(seconds / 60); if (interval > 1) { return interval + "m"; }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) { return interval + "m"; }
 
         return Math.floor(seconds) + "s";
     }
