aa821949ae4ce42999c08751d0a7591d64052835
cmaish <mail@christophermaish.com>
2013-05-17 14:18:52 -0700

    First cut for allowed command

---
+++ JabbR/Commands/AllowedCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using JabbR.Models;
+using JabbR.Services;
+
+namespace JabbR.Commands
+{
+    [Command("allowed", "Show a list of all users allowed in the given room.", "[room]", "global")]
+    public class AllowedCommand : UserCommand
+    {
+        public override void Execute(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
+        {
+            string targetRoomName = args.Length > 0 ? args[0] : callerContext.RoomName;
+
+            if (string.IsNullOrEmpty(targetRoomName) || targetRoomName.Equals("Lobby", StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new InvalidOperationException("Which room?");
+            }
+
+            ChatRoom room = context.Repository.VerifyUserRoom(context.Cache, callingUser, targetRoomName);
+
+            context.NotificationService.ListAllowedUsers(room);
+        }
+    }
+}
