a2d1cf344d925fd16a99e0558b38d93a94255fef
David Fowler <davidfowl@gmail.com>
2013-05-19 00:17:01 -0700

    Work around bug in FormsAuthMiddleware

--- JabbR/Middleware/FixCookieHandler.cs
+++ JabbR/Middleware/FixCookieHandler.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler;
 
 namespace JabbR.Middleware
 {
-    using Microsoft.Owin.Security;
-    using Microsoft.Owin.Security.DataHandler;
     using AppFunc = Func<IDictionary<string, object>, Task>;
 
     public class FixCookieHandler
@@ -29,6 +30,32 @@ namespace JabbR.Middleware
             if (cookies != null && cookies.TryGetValue("jabbr.id", out cookieValue))
             {
                 AuthenticationTicket ticket = _ticketHandler.Unprotect(cookieValue);
+                if (ticket.Extra == null)
+                {
+                    // The forms auth module has a bug where it null refs on a null Extra
+                    var headers = request.Get<IDictionary<string, string[]>>(Owin.Types.OwinConstants.RequestHeaders);
+
+                    var cookieBuilder = new StringBuilder();
+                    foreach (var cookie in cookies)
+                    {
+                        // Skip the jabbr.id cookie
+                        if (cookie.Key == "jabbr.id")
+                        {
+                            continue;
+                        }
+
+                        if (cookieBuilder.Length > 0)
+                        {
+                            cookieBuilder.Append(";");
+                        }
+
+                        cookieBuilder.Append(cookie.Key)
+                                     .Append("=")
+                                     .Append(Uri.EscapeDataString(cookie.Value));
+                    }
+
+                    headers["Cookie"] = new[] { cookieBuilder.ToString() };
+                }
             }
             
             return _next(env);
