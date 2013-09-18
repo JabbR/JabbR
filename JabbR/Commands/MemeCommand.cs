a8e4db6feeabe1cda7e859a66fa49004cd51f129
Pure Krome <github@world-domination.com.au>
2013-09-18 23:27:26 +1000

    I don't always get new commands, but when I do, it's a MEME command. - Added new command: /meme.

---
+++ JabbR/Commands/MemeCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using JabbR.Models;
+using JabbR.Services;
+using Microsoft.AspNet.SignalR;
+
+namespace JabbR.Commands
+{
+    [Command("meme", "Meme_CommandInfo", "meme-type/top-line/bottom-line", "user")]
+    public class MemeCommand : UserCommand
+    {
+        public override void Execute(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
+        {
+            if (String.IsNullOrEmpty(callerContext.RoomName))
+            {
+                throw new HubException(LanguageResources.InvokeFromRoomRequired);
+            }
+
+            if (args == null || args.Length < 1)
+            {
+                throw new HubException(LanguageResources.Meme_DataRequired);
+            }
+
+            if (args.Length != 3)
+            {
+                throw new HubException(LanguageResources.Meme_IncorrectNumberOfArguments);
+            }
+
+            ChatRoom callingRoom = context.Repository.GetRoomByName(callerContext.RoomName);
+            string message = String.Format("http://upboat.me/{0}/{1}/{2}.jpg", args[0], args[1], args[2]);
+
+            context.NotificationService.GenerateMeme(callingUser, callingRoom, message);
+        }
+    }
+}
