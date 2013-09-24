3d26f58da0aa166f6302530ce5e86866ae62ef5c
David Fowler <davidfowl@gmail.com>
2013-09-24 14:37:19 -0700

    Regexes are EVIL! - Simplified the regex as it can make the CPU run at 100%

--- JabbR/Infrastructure/UrlExtractor.cs
+++ JabbR/Infrastructure/UrlExtractor.cs
@@ -7,7 +7,7 @@ namespace JabbR.Infrastructure
 {
     public class UrlExtractor
     {
-        private static Regex urlPattern = new Regex(@"(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static Regex urlPattern = new Regex(@"(?:(?:https?)://|www\.)[^\s]+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public static IList<string> ExtractUrls(string message)
         {
