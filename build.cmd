da3cfdb7f56cb58640b038e9749dfc74d0b027c8
David Fowler <davidfowl@gmail.com>
2013-09-23 23:05:02 -0700

    Remove the vs version flag.

--- build.cmd
+++ build.cmd
@@ -5,4 +5,4 @@ if "%config%" == "" (
 )
 
 .nuget\NuGet.exe restore JabbR.sln -configFile %~dp0\.nuget\NuGet.config -nocache
-msbuild %~dp0\Build\Build.proj /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /p:VisualStudioVersion=11.0
+msbuild %~dp0\Build\Build.proj /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
