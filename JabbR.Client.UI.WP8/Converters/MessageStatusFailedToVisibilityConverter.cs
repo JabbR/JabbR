aa8d64fd74a4838e86087e1639a9e7634c160127
Henrik Andersson <henrik.a.andersson@outlook.com>
2013-10-03 21:38:20 +1000

    Display indicator when a message fails to be sent

--- JabbR.Client.UI.WP8/Converters/MessageStatusFailedToVisibilityConverter.cs
+++ JabbR.Client.UI.WP8/Converters/MessageStatusFailedToVisibilityConverter.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace JabbR.Client.UI.WP8.Converters
 {
-    public class MessageStatusSlowToBoolConverter : IValueConverter
+    public class MessageStatusFailedToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             if(value == null)
             {
-                return false;
+                return Visibility.Collapsed;
             }
 
             switch (value.ToString().ToLower())
             {
-                case "slow":
-                    return true;
+                case "error":
+                    return Visibility.Visible;
                 default:
-                    return false;
+                    return Visibility.Collapsed;
             }
         }
 
---
+++ JabbR.Client.UI.WP8/Converters/MessageStatusSlowToBooleanConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace JabbR.Client.UI.WP8.Converters
+{
+    public class MessageStatusSlowToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if(value == null)
+            {
+                return false;
+            }
+
+            switch (value.ToString().ToLower())
+            {
+                case "slow":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
