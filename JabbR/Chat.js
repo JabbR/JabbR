0a7b972335b26b89cf3d19e8a184ac031d17d537
cmaish <mail@christophermaish.com>
2013-10-09 23:20:38 -0700

    Remove unused showRooms method

--- JabbR/Chat.js
+++ JabbR/Chat.js
@@ -869,36 +869,6 @@
         ui.addNotificationToActiveRoom(message);
     };
 
-    // Helpish commands
-    //TODO: remove, not called anywhere.
-    chat.client.showRooms = function (rooms) {
-        if (!rooms.length) {
-            ui.addListToActiveRoom('Rooms', [utility.getLanguageResource('Chat_NoRoomsAvailable')]);
-        }
-        else {
-            // sort rooms by count descending then name
-            var sorted = rooms.sort(function (a, b) {
-                if (a.Closed && !b.Closed) {
-                    return 1;
-                } else if (b.Closed && !a.Closed) {
-                    return -1;
-                }
-
-                if (a.Count > b.Count) {
-                    return -1;
-                } else if (b.Count > a.Count) {
-                    return 1;
-                }
-
-                return a.Name.toString().toUpperCase().localeCompare(b.Name.toString().toUpperCase());
-            });
-
-            ui.addListToActiveRoom('Rooms', $.map(sorted, function () {
-                return this.Name + ' (' + this.Count + ')';
-            }));
-        }
-    };
-
     chat.client.showCommands = function () {
         ui.showHelp();
     };
