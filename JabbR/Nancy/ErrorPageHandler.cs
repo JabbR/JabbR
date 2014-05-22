5a44b736a5e3b56f31b7fd9c20ec266a322daba1
cmaish <mail@christophermaish.com>
2014-05-22 10:54:41 -0700

    Add error page, resolve #586

---
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -0,0 +1,35 @@
+﻿using Nancy;
+using Nancy.ErrorHandling;
+using Nancy.ViewEngines;
+
+namespace JabbR.Nancy
+{
+    public class ErrorPageHandler : DefaultViewRenderer, IStatusCodeHandler
+    {
+        public ErrorPageHandler(IViewFactory factory)
+            : base(factory)
+        {
+        }
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            // only handle 40x and 50x
+            return (int)statusCode >= 400;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            var response = RenderView(
+                context, 
+                "errorPage", 
+                new 
+                { 
+                    Error = statusCode,
+                    ErrorCode = (int)statusCode 
+                });
+
+            response.StatusCode = statusCode;
+            context.Response = response;
+        }
+    }
+}
